83ef9d5eaf8e76fb677ba77795c48f7a96997dbe
Joshua Lo <33582835+joshrclo@users.noreply.github.com>
2021-01-13 20:44:46 -0500

    Update pascal_coco.py

--- datasets/pascal_coco.py
+++ datasets/pascal_coco.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
-COCO dataset which returns image_id for evaluation.
-Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
+PASCAL VOC 2012 dataset with COCO annotations format.
+Based on coco.py.
 """
 from pathlib import Path
 
